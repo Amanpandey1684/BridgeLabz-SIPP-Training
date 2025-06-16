e6e2fc5aab997d9f4a04f58290de8742e4f745bd
Aman Pandey <aman.pandey_cs22@gla.ac.in>
2025-06-16 10:53:55 +0530

    [Aman Pandey].add COntrolFlows for Day 3

---
+++ ControlFlows/LeapYears.java
@@ -0,0 +1,25 @@
+package ControlFlows;
+
+import java.util.Scanner;
+
+public class LeapYears {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a year (>= 1582): ");
+        int year = sc.nextInt();
+
+        if (year < 1582) {
+            System.out.println("The program works for years from 1582 onwards (Gregorian calendar).");
+        } else if (year % 400 == 0) {
+            System.out.println(year + " is a Leap Year");
+        } else if (year % 100 == 0) {
+            System.out.println(year + " is Not a Leap Year");
+        } else if (year % 4 == 0) {
+            System.out.println(year + " is a Leap Year");
+        } else {
+            System.out.println(year + " is Not a Leap Year");
+        }
+
+        sc.close();
+    }
+}
