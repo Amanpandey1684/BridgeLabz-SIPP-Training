083c968bcfc9d01624b17e0095e4278272520421
Aman Pandey <aman.pandey_cs22@gla.ac.in>
2025-06-24 09:36:23 +0530

    [Deleted files]

--- JavaFundamentals/convert_in_miles.java
+++
@@ -1,7 +0,0 @@
-public class convert_in_miles {
-    public static void main(String[] args){
-        double Km=10.8;
-        double miles= Km*1.6;
-        System.out.println("The distance"+ Km+"Km in miles is" + miles );
-    }
-}
